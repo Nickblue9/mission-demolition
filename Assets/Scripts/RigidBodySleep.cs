fe12ec93ea6c05365b22d5eb42c3690655ef515f
Nick Edwards <nicholas6598@gmail.com>
2020-10-21 17:52:27 -0400

    Reloading for second shot

--- Assets/Scripts/RigidBodySleep.cs
+++ Assets/Scripts/RigidBodySleep.cs
@@ -1,18 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class RigidBodySleep : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+public class RigidBodySleep : MonoBehaviour{
+    void Start(){
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if(rb != null)rb.Sleep();
     }
 }
